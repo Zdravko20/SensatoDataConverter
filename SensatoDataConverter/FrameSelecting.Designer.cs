fc818c2c24e83007a72decdbb2fbf8196fe44a6a
Zdravko20 <buckzful@gmail.com>
2016-09-09 18:39:27 +0300

    Add new icon

--- SensatoDataConverter/FrameSelecting.Designer.cs
+++ SensatoDataConverter/FrameSelecting.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrameSelecting));
             this.frame1Checkbox = new System.Windows.Forms.CheckBox();
             this.frame2Checkbox = new System.Windows.Forms.CheckBox();
             this.frame13Checkbox = new System.Windows.Forms.CheckBox();
@@ -399,6 +400,7 @@
             this.Controls.Add(this.frame1Checkbox);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FrameSelecting";
             this.Text = "FrameSelecting";
